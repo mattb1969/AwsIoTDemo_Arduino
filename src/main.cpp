ea78c77851526a92d1013e24cbd5e2157989c685
mattb1969 <matthew@mlbennett.com>
2021-04-13 08:49:03 +0100

    first working version on my code base.

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,33 @@
+
+
 #include <Arduino.h>
-#include <arduino_secrets.h>
-#include <MQTT.h>
-#include <WiFi.h>
+
+#include <ArduinoBearSSL.h>
+#include <ArduinoECCX08.h>
+#include <ArduinoMqttClient.h>
+//#include <MQTT.h>
+//#include <WiFi.h>
+//#include <WiFiSSLClient.h>
+#include <WiFiNINA.h>
+
+#include "arduino_secrets.h"
 
 #include <ArduinoJson.h>
 
 // The MQTT topics that this device should publish/subscribe
-#define AWS_IOT_PUBLISH_TOPIC "esp32/pub"
-#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+#define AWS_IOT_PUBLISH_TOPIC "arduino/outgoing"
+#define AWS_IOT_SUBSCRIBE_TOPIC "arduino/incoming"
 
-MQTTClient client = MQTTClient(256);
+//MQTTClient client; // = MQTTClient(256);
+//WiFiSSLClient wifiClient;
+//BearSSLClient sslClient(wifiClient);            // Used for SSL/TLS connection, integrates with ECC508
+WiFiClient    wifiClient;                   // Used for the TCP socket connection
+BearSSLClient sslClient(wifiClient);        // Used for SSL/TLS connection, integrates with ECC508
+MqttClient    client(sslClient);
 
 bool connectWifi()
 {
-
-    //char ssid[] = WIFI_SSID;   // your network SSID (name)
-    //char pass[] = WIFI_PASSWORD;   // your network password (use for WPA, or use as key for WEP)
-    int status = WL_IDLE_STATUS; // the WiFi radio's status
+    int status = false; // the WiFi radio's connection status
 
     // check for the WiFi module:
     if (WiFi.status() == WL_NO_MODULE)
@@ -39,14 +50,15 @@ bool connectWifi()
     }
 
     // attempt to connect to WiFi network:
-    while (status != WL_CONNECTED)
+    while (WiFi.status() != WL_CONNECTED)
     {
         Serial.print("Attempting to connect to WPA SSID: ");
         Serial.println(WIFI_SSID);
         // Connect to WPA/WPA2 network:
         status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
     }
-    return true;
+
+    return status;
 }
 
 void printMacAddress(byte mac[])
@@ -90,21 +102,140 @@ void printCurrentWifi()
     Serial.println();
 }
 
+unsigned long getTime() {
+    // get the current time from the WiFi module  
+    return WiFi.getTime();
+}
+
+void messageHandler(int messageSize)
+{
+      // we received a message, print out the topic and contents
+  Serial.print("Received a message with topic '");
+  Serial.print(client.messageTopic());
+  Serial.print("', length ");
+  Serial.print(messageSize);
+  Serial.println(" bytes:");
+
+  // use the Stream interface to print the contents
+  while (client.available()) {
+    Serial.print((char)client.read());
+  }
+  Serial.println();
+
+  Serial.println();
+
+}
+
+void publishMessage()
+{
+    //StaticJsonDocument<200> doc;
+    //doc["time"] = millis();
+    //doc["sensor_a0"] = analogRead(0);
+    //char jsonBuffer[512];
+    //serializeJson(doc, jsonBuffer); // print to client
+
+    //client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+
+    // send message, the Print interface can be used to set the message contents
+    client.beginMessage(AWS_IOT_PUBLISH_TOPIC);
+    client.print("hello ");
+    client.print(millis());
+    client.endMessage();
+}
+
+bool connectAws()
+{
+    int status = false;
+    Serial.println("Connecting to AWS");
+
+        // checks to  see if the crypto chip is working
+    if (!ECCX08.begin()) {
+        Serial.println("No ECCX08 present!");
+        while (1);
+    }
+    else
+    {
+        Serial.println("ECCX08 copnnected");
+    }
+
+    // Set a callback to get the current time
+    // used to validate the servers certificate
+    ArduinoBearSSL.onGetTime(getTime); //WiFi.getTime());
+
+    // Set the ECCX08 slot to use for the private key
+    // and the accompanying public certificate for it
+    sslClient.setEccSlot(0, SECRET_CERTIFICATE); //certificate);
+
+    return status;
+}
+
+bool connectThing() {
+    int status = false;
+
+    // Optional, set the client id used for MQTT,
+    // each device that is connected to the broker
+    // must have a unique client id. The MQTTClient will generate
+    // a client id for you based on the millis() value if not set
+    //
+    // client.setId("clientId");
+
+    Serial.println("Connecting thing to AWS");
+
+    while (!client.connect(AWS_IOT_ENDPOINT, 8883)) //THINGNAME))
+    {
+        Serial.print(".");
+        delay(100);
+    }
+
+    if (!client.connected())
+    {
+        Serial.println("AWS IoT Timeout!");
+        status = false;
+    }
+    else {
+        Serial.println("Connected to AWS");
+        status = true;
+    }
+
+    return status;
+}
+
+
 void setup()
 {
 
-    Serial.begin(9600);
+    Serial.begin(115200);
     while (!Serial)
     {
         ; // wait for serial port to connect. Needed for native USB port only
     }
 
     connectWifi();
+
+    connectAws();
+
+    Serial.println("Setting up MQTT receiver");
+    // Set the message callback, this function is
+    // called when the MQTTClient receives a message
+    client.onMessage(messageHandler);
+
+    connectThing();
+
+    Serial.println("Subscribing to the topic");
+    // Subscribe to a topic
+    client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+
+
 }
 
 void loop()
 {
-    delay(1000);
     Serial.println("Wifi status: ");
     printCurrentWifi();
+
+    // poll for new MQTT messages and send keep alives
+    client.poll();
+
+    publishMessage();
+    delay(5000);
 }
